7483e5a0f6f7a57404ba5596a35f6dc35f8b75ec
suzuki <tunakyonn@gmail.com>
2017-05-04 21:02:02 +0900

    Add exercise-equivalent-binary-trees.go

--- concurrency/exercise-equivalent-binary-trees.go
+++ concurrency/exercise-equivalent-binary-trees.go
@@ -24,12 +24,37 @@ func Walk(t *tree.Tree, ch chan int) {
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+
+	for {
+		i1, ok1 := <-ch1
+		i2, ok2 := <-ch2
+
+		if i1 != i2 || ok1 != ok2 {
+			return false
+		}
+
+		if !ok1 {
+			break
+		}
+	}
+	return true
+}
 
 func main() {
+	// Walk Test
 	ch := make(chan int)
 	go Walk(tree.New(1), ch)
 	for i := range ch {
 		fmt.Println(i)
 	}
+
+	//Same Test
+	fmt.Println(Same(tree.New(1), tree.New(1))) //true
+	fmt.Println(Same(tree.New(1), tree.New(2))) //false
 }
